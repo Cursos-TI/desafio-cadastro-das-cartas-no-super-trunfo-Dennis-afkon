1eedf52a2f17c9855dd0dc8d2c21b59dc08bb2a4
Dennis-afkon <202501455221@alunos.estacio.br>
2025-08-16 14:29:55 +0000

    Desafio nivel aventureiro terminado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,9 @@ typedef struct {
     float area;
     float pib;
     int pontosTuristicos;
+    float densidade_populacional;
+    float pib_per_capita;
+
 } Carta;
 
 void limparEntrada() {
@@ -55,6 +58,12 @@ int main() {
     scanf(" %c", &carta2.estado);
     limparEntrada();
 
+         //calculando a densidade e o pib per capita da carta 1//
+    carta1.densidade_populacional = carta1.populacao / carta1.area;
+    carta1.pib_per_capita = (carta1.pib * 100000000) / carta1.populacao;
+
+    printf("\n");
+
     printf("Código (ex: A01): ");
     fgets(carta2.codigo, sizeof(carta2.codigo), stdin);
     carta2.codigo[strcspn(carta2.codigo, "\n")] = '\0';
@@ -75,6 +84,11 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &carta2.pontosTuristicos);
 
+
+         //calculando a densidade e o pib per capita da carta 2//
+    carta2.densidade_populacional = carta2.populacao / carta2.area;
+    carta2.pib_per_capita = (carta2.pib * 100000000) / carta2.populacao;
+
     // Exibição
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", carta1.estado);
@@ -84,6 +98,8 @@ int main() {
     printf("Área: %.2f km²\n", carta1.area);
     printf("PIB: %.2f bilhões de reais\n", carta1.pib);
     printf("Número de Pontos Turísticos: %d\n", carta1.pontosTuristicos);
+    printf("Densidade Populacional: %.2f hab/km²\n", carta1.densidade_populacional);
+    printf("PIB per Capita: %.2f reais\n", carta1.pib_per_capita);
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", carta2.estado);
@@ -93,6 +109,8 @@ int main() {
     printf("Área: %.2f km²\n", carta2.area);
     printf("PIB: %.2f bilhões de reais\n", carta2.pib);
     printf("Número de Pontos Turísticos: %d\n", carta2.pontosTuristicos);
+    printf("Densidade Populacional: %.2f hab/km²\n", carta2.densidade_populacional);
+    printf("PIB per Capita: %.2f reais\n", carta2.pib_per_capita);
 
     return 0;
 }
